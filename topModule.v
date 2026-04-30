3d666514faa7f7e75a90302c34a8b49ad0a1491e
Janons <candogrull@hotmail.com>
2026-05-01 00:48:47 +0200

    Improved the Uart Logic

--- topModule.v
+++ topModule.v
@@ -1,32 +1,81 @@
+`default_nettype none
+
 module top (
-    input  wire i_clk,      // Pin 52 (27MHz)
-    input  wire i_reset_n,  // Pin 4 (The "S1" button on the board)
-    output wire o_uart_tx   // Pin 17
+    input  wire i_clk,
+    input  wire i_reset_n,
+    output wire o_uart_tx,
+    output wire o_led
 );
 
-    wire [30:0] setup = 31'h700000EA; // 115200 baud @ 27MHz
-    reg  [24:0] counter;
-    reg         wr_pulse;
+    // --- 1. Power-on Reset & Timer (500ms) ---
+    reg [3:0]  reset_cnt = 0;
+    wire       power_on_reset = (reset_cnt < 4'hF);
+    wire [30:0] simple_setup = 31'h400000EA;
+    always @(posedge i_clk) if (power_on_reset) reset_cnt <= reset_cnt + 1'b1;
 
-    // Send a character roughly every 0.5 seconds
+    wire       global_reset = power_on_reset || !i_reset_n;
+    reg [23:0] timer_cnt = 0;
+    reg        wr_pulse;
+
     always @(posedge i_clk) begin
-        counter <= counter + 1'b1;
-        wr_pulse <= (counter == 24'h1FFFFFF); 
+        if (global_reset) begin
+            timer_cnt <= 0;
+            wr_pulse  <= 0;
+        end else if (timer_cnt >= 24'd13_500_000) begin // 500ms
+            timer_cnt <= 0;
+            wr_pulse  <= 1;
+        end else begin
+            timer_cnt <= timer_cnt + 1'b1;
+            wr_pulse  <= 0;
+        end
     end
 
-    txuart my_tx (
+    // --- 2. The String Logic ---
+    reg [3:0] char_index = 0;
+    reg [7:0] string_rom [0:11];
+    reg       sending = 0;
+
+    initial begin
+        string_rom[0] = "H"; string_rom[1] = "e"; string_rom[2] = "l";
+        string_rom[3] = "l"; string_rom[4] = "o"; string_rom[5] = " ";
+        string_rom[6] = "W"; string_rom[7] = "o"; string_rom[8] = "r";
+        string_rom[9] = "l"; string_rom[10]= "d"; string_rom[11]= "!";
+    end
+
+    always @(posedge i_clk) begin
+        if (global_reset) begin
+            char_index <= 0;
+            sending    <= 0;
+        end else if (wr_pulse) begin
+            sending    <= 1; // Start the sequence
+        end else if (sending && !o_busy) begin
+            if (char_index < 11)
+                char_index <= char_index + 1'b1;
+            else begin
+                char_index <= 0;
+                sending    <= 0; // Finished the string
+            end
+        end
+    end
+
+    // --- 3. UART Instance ---
+    wire o_busy;
+    txuart #(
+        .INITIAL_SETUP(31'd234) // 115200 baud with a 50MHz clock
+    ) my_tx (
         .i_clk(i_clk),
-        .i_reset(!i_reset_n), // S1 is active-low, we need active-high
-        .i_setup(setup),
+        .i_reset(global_reset),
+        .i_setup(31'd235),
         .i_break(1'b0),
-        .i_wr(wr_pulse),
-        .i_data(8'h47),       // ASCII 'G'
-        .i_cts_n(1'b0),       // Always ready
+        // We only trigger 'i_wr' when we are in the 'sending' state 
+        // AND the UART isn't already busy with a previous character.
+        .i_wr(sending && !o_busy), 
+        .i_data(string_rom[char_index]),
+        .i_cts_n(1'b0),
         .o_uart_tx(o_uart_tx),
-        .o_busy()
+        .o_busy(o_busy)
     );
 
+    assign o_led = o_uart_tx;
+
 endmodule
-
-
-//just used to check the code of ZIP CPU
--- transmitter.v
+++ transmitter.v
@@ -1,198 +1,95 @@
-`default_nettype	none
-//
-// }}}
-module txuart #(
-		// {{{
-		parameter	[30:0]	INITIAL_SETUP = 31'd868,
-		//
-		localparam 	[3:0]	TXU_BIT_ZERO  = 4'h0,
-		localparam 	[3:0]	TXU_BIT_ONE   = 4'h1,
-		localparam 	[3:0]	TXU_BIT_TWO   = 4'h2,
-		localparam 	[3:0]	TXU_BIT_THREE = 4'h3,
-		// localparam 	[3:0]	TXU_BIT_FOUR  = 4'h4,
-		// localparam 	[3:0]	TXU_BIT_FIVE  = 4'h5,
-		// localparam 	[3:0]	TXU_BIT_SIX   = 4'h6,
-		localparam 	[3:0]	TXU_BIT_SEVEN = 4'h7,
-		localparam 	[3:0]	TXU_PARITY    = 4'h8,
-		localparam 	[3:0]	TXU_STOP      = 4'h9,
-		localparam 	[3:0]	TXU_SECOND_STOP = 4'ha,
-		//
-		localparam 	[3:0]	TXU_BREAK     = 4'he,
-		localparam 	[3:0]	TXU_IDLE      = 4'hf
-		// }}}
-	) (
-		// {{{
-		input	wire		i_clk, i_reset,
-		input	wire	[30:0]	i_setup, //channging the UART BAUD RATE etc
-            // i_setup[30] = 0 to use hardware flow control, 1 to ignore it
-            // i_setup[29:28] = number of data bits (00=5, 01=6, 10=7, 11=8)
-            // i_setup[27] = 1 to use two stop bits, 0 for one stop bit
-            // i_setup[26] = 1 to use parity, 0 for no parity
-            // i_setup[25] = 1 to use fixed parity, 0 for calculated parity
-            // i_setup[24] = if fixed parity, this is the value of the parity bit.
-            //               if calculated parity, this is the odd/even bit (1 for odd)
-            // i_setup[23:0] = number of clocks per baud (e.g. 868 for 115200 baud with a 100MHz clock)
 
-		input	wire		i_break,
-		input	wire		i_wr,
-		input	wire	[7:0]	i_data,
-		// Hardware flow control Ready-To-Send bit.  Set this to one to
-		// use the core without flow control.  (A more appropriate name
-		// would be the Ready-To-Receive bit ...)
-		input	wire		i_cts_n,
-		// And the UART input line itself
-		output	reg		o_uart_tx,
-		// A line to tell others when we are ready to accept data.  If
-		// (i_wr)&&(!o_busy) is ever true, then the core has accepted a
-		// byte for transmission.
-		output	wire		o_busy
-		// }}}
-	);
+`timescale 1ns / 1ps
 
-	// Signal declarations
-	// {{{
-	wire	[27:0]	clocks_per_baud, break_condition;
-	wire	[1:0]	i_data_bits, data_bits;
-	wire		use_parity, parity_odd, dblstop, fixd_parity,
-			fixdp_value, hw_flow_control, i_parity_odd;
-	reg	[30:0]	r_setup;
-	assign	clocks_per_baud = { 4'h0, r_setup[23:0] };
-	assign	break_condition = { r_setup[23:0], 4'h0 };
-	assign	hw_flow_control = !r_setup[30];
-	assign	i_data_bits     =  i_setup[29:28];
-	assign	data_bits       =  r_setup[29:28];
-	assign	dblstop         =  r_setup[27];
-	assign	use_parity      =  r_setup[26];
-	assign	fixd_parity     =  r_setup[25];
-	assign	i_parity_odd    =  i_setup[24];
-	assign	parity_odd      =  r_setup[24];
-	assign	fixdp_value     =  r_setup[24];
+module uart_tx #(
 
-	reg	[27:0]	baud_counter;
-	reg	[3:0]	state;
-	reg	[7:0]	lcl_data;
-	reg	 r_busy, zero_baud_counter, last_state;
+	parameter data_width = 8,
+)
 
+(input wire clk,
+input wire rst_n,
+input wire tx_valid,
+input wire tx_ready,
+input wire [data_width-1:0] tx_data,
+input wire [15:0] prescale,
+)
+reg [3:0] txState = 0;
+reg [24:0] txCounter = 0;
+reg [7:0] dataOut = 0;
+reg txPinRegister = 1;
+reg [2:0] txBitNumber = 0;
+reg [3:0] txByteCounter = 0;
 
-	wire ck_cts;
-// If setup bit 30 is 1, we ignore CTS and stay "Ready".
-// If it's 0, we check i_cts_n (inverted because it's active-low).
-	assign ck_cts = (r_setup[30]) ? 1'b1 : !i_cts_n;
+//state machine states
+localparam TX_STATE_IDLE = 0;
+localparam TX_STATE_START_BIT = 1;
+localparam TX_STATE_WRITE = 2;
+localparam TX_STATE_STOP_BIT = 3;
+localparam TX_STATE_DEBOUNCE = 4;
 
-
-	initial	r_busy = 1'b1;
-	initial	state  = TXU_IDLE;
-	always @(posedge i_clk)
-	if (i_reset)
+//states
+always @(posedge clk or negedge rst_n) begin
+	if (rst_n)
 	begin
-		r_busy <= 1'b1;
-		state <= TXU_IDLE;
-	end else if (i_break)
-	begin
-		state <= TXU_BREAK;
-		r_busy <= 1'b1;
-	end else if (!zero_baud_counter)
-	begin // r_busy needs to be set coming into here
-		r_busy <= 1'b1;
-	end else if (state == TXU_BREAK)
-	begin
-		state <= TXU_IDLE;
-		r_busy <= !ck_cts;
-	end else if (state == TXU_IDLE)	// STATE_IDLE
-	begin
-		if ((i_wr)&&(!r_busy))
-		begin	// Immediately start us off with a start bit
-			r_busy <= 1'b1;
-			case(i_data_bits)
-			2'b00: state <= TXU_BIT_ZERO;
-			2'b01: state <= TXU_BIT_ONE;
-			2'b10: state <= TXU_BIT_TWO;
-			2'b11: state <= TXU_BIT_THREE;
-			endcase
-		end else begin // Stay in idle
-			r_busy <= !ck_cts;
-		end
-	end else begin
-		// One clock tick in each of these states ...
-		// baud_counter <= clocks_per_baud - 28'h01;
-		r_busy <= 1'b1;
-		if (state[3] == 0) // First 8 bits
-		begin
-			if (state == TXU_BIT_SEVEN)
-				state <= TXU_STOP;
-			else
-				state <= state + 1;
-		end else if (state == TXU_PARITY)
-		begin
-			state <= TXU_STOP;
-		end else if (state == TXU_STOP)
-		begin // two stop bit(s)
-			if (dblstop)
-				state <= TXU_SECOND_STOP;
-			else
-				state <= TXU_IDLE;
-		end else // `TXU_SECOND_STOP and default:
-		begin
-			state <= TXU_IDLE; // Go back to idle
-			// Still r_busy, since we need to wait
-			// for the baud clock to finish counting
-			// out this last bit.
-		end
-	end 
+		txState <= TX_STATE_IDLE;
+		txCounter <= 0;
+		dataOut <= 0;
+		txPinRegister <= 1;
+		txBitNumber <= 0;
+		txByteCounter <= 0;
+	end
+	else begin
+		case (txState)
+			TX_STATE_IDLE: begin
+				if (tx_valid) begin
+					txState <= TX_STATE_START_BIT;
+					txCounter <= 0;
+					dataOut <= {1'b1, tx_data}; // Add stop bit
+					txPinRegister <= 0; // Start bit
+					tx_ready <= 1;
+				end
+			else begin
+				txPinRegister <= 1;
+				tx_ready <= 0;
+			end
+			TX_STATE_START_BIT: begin
+				txPinRegister <= 0; // Start bit
+				if ((txCounter + 1) == (prescale << 3)) begin
+					txState <= TX_STATE_WRITE;
+					//dataOut <= {1'b1, tx_data}; // Add stop bit
+					txCounter <= 0;
+					txBitNumber <= 0;
+					
+				end
+				else begin
+					txCounter <= txCounter + 1;
 
-	assign	o_busy = (r_busy);
+				end
+			end
 
+			TX_STATE_WRITE: begin
+				txPinRegister <= dataOut[0];
+				if (txCounter + 1) == (prescale <<3) begin
+					if (txBitNumber == 7) begin
+						txState <= TX_STATE_STOP_BIT;
+						txCounter <= 0;
+						txBitNumber <= 0;
+					end else begin
+						txState <= TX_STATE_WRITE;
+						dataOut <= dataOut >> 1; // Shift to get the next bit
+						txCounter <= 0;
+						txBitNumber <= txBitNumber + 1;
+					end	
+				end else begin
+					tx_counter <= txCounter + 1;
+				end
+				
+				end
+			end
 
-	initial	r_setup = INITIAL_SETUP;
-	always @(posedge i_clk)
-	if (!o_busy)
-		r_setup <= i_setup;
-
-	always @(posedge i_clk)
-	if (!r_busy)
-		lcl_data <= i_data;
-	else if (zero_baud_counter)
-		lcl_data <= { 1'b0, lcl_data[7:1] };
-	// }}}
-
-
-	//output logic
-	initial	o_uart_tx = 1'b1;
-	always @(posedge i_clk)
-	if (i_reset)
-		o_uart_tx <= 1'b1;
-	else if ((i_break)||((i_wr)&&(!r_busy)))
-		o_uart_tx <= 1'b0;
-	else if (zero_baud_counter)
-		casez(state)
-		4'b0???:	o_uart_tx <= lcl_data[0];
-		default:	o_uart_tx <= 1'b1;
 		endcase
 
+end
 
 
-	initial	last_state = 1'b0;
-	always @(posedge i_clk)
-	if (i_reset)
-		last_state <= 1'b0;
-	else if (dblstop)
-		last_state <= (state == TXU_SECOND_STOP);
-	else
-		last_state <= (state == TXU_STOP);
-
-
-			// This is the missing baud counter logic
-	always @(posedge i_clk)
-	begin
-		if (i_reset || !r_busy)
-			baud_counter <= clocks_per_baud - 28'h1;
-		else if (zero_baud_counter)
-			baud_counter <= clocks_per_baud - 28'h1;
-		else
-			baud_counter <= baud_counter - 28'h1;
-
-		// This tells the rest of the code when one bit-width of time has passed
-		zero_baud_counter <= (baud_counter == 28'h1);
-	end
-
-    endmodule
+endmodule
