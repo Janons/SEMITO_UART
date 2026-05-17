894346e897e2329a499fdeb48b50ca8491ef7536
Kaya Oyman <oymankaya@gmail.com>
2026-05-17 17:12:25 +0200

    fifo implemented

--- src/fifo.v
+++ src/fifo.v
@@ -1,34 +1,92 @@
-module fifo (parameter DATA_W = 8)(
+module fifo #(parameter DATA_W = 8, 
+            parameter ADDR_W = 6
+    )(
     input wire clk,
-    input wire i_wr,
-    input wire i_data,
-    input reg i_rd,
-    output reg o_data,
-    output reg o_empty
+    input wire rst_n,
+
+    input wire wr,
+    input wire rd,
+    output wire [DATA_W-1 : 0] rd_data
+    input wire [DATA_W-1 : 0] wr_data 
+    
+    output wire empty,
+    output wire full
 );
 
-//fifo memory
-reg[7:0] fifo_mem [0:63];
-reg [6:0] wr_addr;
-reg[6:0] rd_addr;
+//fifo memory, we chose that 64 bytes are enough
+reg[DATA_W : 0] fifo_mem [2**ADDR_W - 1];
+reg[ADDR_W-1 : 0] wr_addr_cur, wr_addr_succ, wr_addr_next;
+reg[ADDR_W-1 : 0] rd_addr_cur, rd_addr_succ, rd_addr_next;
+reg[DATA_W : 0] read_reg;
+reg full_reg, empty_reg, full_next, empty_next;
+wire wr_en;
+wire rd_en;
 
 //conditions
-assign w_wr = i_wr && !o_full;
-assign w_rd = i_rd && !o_empty;
+assign wr_en = wr && ~full;
+assign rd_en = rd && ~empty;
+
+always @(posedge clk) begin
+    if (wr_en) begin
+        fifo_mem[wr_addr_cur] <= wr_data;
+    end else if (rd_en) begin
+        read_reg <= fifo_mem[rd_addr_cur];
+    end
+end
+assign rd_data = read_reg;
 
 //fifo memory
 initial wr_addr = 0;
-always @(posedge clk) begin
-    if (w_wr)
-        wr_addr <= wr_addr +1 // if we do writing, we increment the writing by one
+always @(posedge clk, posedge rst_n) begin
+    if (rst_n) begin
+        wr_addr_cur <= 0;
+        rd_addr_cur <= 0;
+        full_reg <= 0;
+        empty_reg <= 0;
+    end else if (clk) begin
+        wr_addr_cur <= wr_addr_next;
+        rd_addr_cur <= rd_addr_next;
+        full_reg <= full_next;
+        empty_reg <= empty_next;
+    end
 end
 
-always @(posedge clk) begin
-
-    if (w_wr)
-        fifo_mem[wr_addr] <= i_data;
-
-
-    end
-
+//now the logic for the next clock cycle prep
+always @(*) begin
+    wr_addr_succ = wr_addr_cur + 1;
+    rd_addr_succ = rd_addr_cur + 1;
+    wr_addr_next = wr_addr_cur; //this allows us to do the following: if we don't read/write, the next values are untouched
+    rd_addr_next = rd_addr_cur; //if we read/write, we modify the next values 
+    full_next = full_reg;
+    empty_next = empty_reg; //same logic for these
+    case({rd, wr}) 
+        2'b01: //we write
+            begin   
+                if (~full_next) begin
+                    wr_addr_next = wr_addr_succ;
+                    empty_next = 1'b0;
+                    if (wr_addr_next == rd_addr_cur) begin
+                        full_next = 1'b1;
+                    end
+                end
+            end
+        2'b10: 
+            begin //we read
+                if (~empty_next) begin
+                    rd_addr_next = rd_addr_succ;
+                    full_next = 1'b0;
+                    if (rd_addr_next == wr_addr_cur) begin
+                        empty_next = 1'b0;
+                    end
+                end
+            end
+        2'b11: //we read AND write
+            begin
+                rd_addr_next = rd_addr_succ;
+                wr_addr_next = wr_addr_succ;
+            end
+    endcase
+end
+assign full = full_reg;
+assign empty = empty_reg;
 endmodule
--- src/topModule.v
+++ src/topModule.v
@@ -13,6 +13,18 @@ wire       tx_ready;
 wire [7:0] rx_data;    // ← internal wire, not port
 wire       rx_ready;   // ← internal wire, not port
 
+wire [7:0] rx_fifo_dout; // Out of FIFO
+wire       rx_fifo_empty;
+wire       rx_fifo_full;
+wire       rx_fifo_rd;   // Tell FIFO to output next byte
+
+reg  [7:0] tx_fifo_din = 0; // Data to push into TX FIFO
+reg        tx_fifo_wr = 0; // Push command for TX FIFO
+wire [7:0] tx_fifo_dout;     // Data coming out of TX FIFO
+wire       tx_fifo_empty;
+wire       tx_fifo_full;
+wire       tx_fifo_rd;       // Pop command for TX FIFO
+
 reg [7:0] message [0:1];
 reg [1:0] byteIndex = 0;
 
@@ -21,11 +33,37 @@ initial begin
     message[1] = 8'h69;  // 'i'
 end
 
+//tx fifo signals
+fifo #(.DATA_W(8), .ADDR_W(6)) tx_fifo_inst (
+    .clk(clk),
+    .rst_n(rst_n),
+    .wr(tx_fifo_wr),
+    .rd(tx_fifo_rd),
+    .wr_data(tx_fifo_din),
+    .rd_data(tx_fifo_dout),
+    .empty(tx_fifo_empty),
+    .full(tx_fifo_full)
+);
+
+//rx fifo signals
+fifo #(.DATA_W(8), .ADDR_W(6)) rx_fifo_inst (
+    .clk(clk),
+    .rst_n(rst_n),
+    .wr(rx_ready),
+    .rd (rx_fifo_rd),
+    .wr_data(rx_data),
+    .rd_data(rx_fifo_dout),
+    .empty(rx_fifo_empty),
+    .full(rx_fifo_full)
+);
+assign tx_fifo_rd = (!tx_fifo_empty && tx_ready);
+assign tx_valid   = tx_fifo_rd;
+
 txuart #(.data_width(8)) uart_inst (
     .clk      (clk),
     .rst_n    (rst_n),
     .tx_valid (tx_valid),
-    .tx_data  (tx_data),
+    .tx_data  (tx_fifo_dout), 
     .clk_per_baud (16'd234),    // ← fixed: 27MHz / (115200 × 8) = 29
     .tx_ready (tx_ready),
     .txd      (uart_txd)
@@ -47,24 +85,25 @@ localparam WAIT = 2;
 always @(posedge clk or negedge rst_n) begin
     if (!rst_n) begin
         byteIndex   <= 0;
-        tx_valid    <= 0;
-        tx_data     <= 0;
+        tx_fifo_wr  <= 0;
+        tx_fifo_din <= 0;
         senderState <= LOAD;
     end else begin
         case (senderState)
             LOAD: begin
-                tx_data     <= message[byteIndex];
-                tx_valid    <= 1;
+                if (!tx_fifo_full) //in the old version we wrote directly to uart, here we first write to fifo 
+                tx_fifo_din <= message[byteIndex];
+                tx_fifo_wr <= 1;
                 senderState <= SEND;
             end
             SEND: begin
-                if (tx_ready) begin
-                    tx_valid    <= 0;
+                if (tx_fifo_wr) begin
+                    tx_fifo_wr  <= 0;
                     senderState <= WAIT;
                 end
             end
             WAIT: begin
-                if (!tx_ready) begin    // ← wait for uart to go idle
+                if (!tx_fifo_wr) begin    // ← wait for uart to go idle
                     if (byteIndex == 1)
                         byteIndex <= 0;
                     else
@@ -81,7 +120,7 @@ reg led_reg = 0;
 always @(posedge clk) begin   // ← fixed: clk not i_clk
     if (!rst_n)
         led_reg <= 0;
-    else if (rx_ready && rx_data == "A")
+    else if (rx_fifo_rd && rx_fifo_dout == "A")
         led_reg <= ~led_reg;
 end
 
